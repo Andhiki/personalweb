1231d7c12f68ddb7dec5fe01e57e147c889346fa
Andhiki <77545971+Andhiki@users.noreply.github.com>
2024-04-04 23:51:55 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html","./build/js/*.js"],
+  theme: {
+    fontFamily: {
+      'anton' : ['"Anton"', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'paper-white': '#dbd5cd',
+        'fruit-orange': '#F24b24',
+      },
+      screens: {
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
+        'tallscreen': { 'raw': '(max-aspect-ratio: 13/20)' },
+      },
+      keyframe: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)' },
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      },
+    },
+  },
+  plugins: [],
+}
+
